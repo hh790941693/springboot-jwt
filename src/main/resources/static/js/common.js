1d40d24d312159e3c0b28c29e0618728a303579c
hch <547495788@qq.com>
2020-05-14 10:24:02 +0800

    优化

--- src/main/resources/static/js/common.js
+++
@@ -1,5 +0,0 @@
-let protocol = window.location.protocol;
-let host = window.location.host;
-
-//上传文件接口地址
-uploadUrl = protocol+"//"+host+"/upload/app";
--- src/main/resources/static/js/viewjs/common.js
+++ src/main/resources/static/js/viewjs/common.js
@@ -1,3 +1,9 @@
+let protocol = window.location.protocol;
+let host = window.location.host;
+
+//上传文件接口地址
+uploadUrl = protocol+"//"+host+"/upload/app";
+
 function getCurrentTime()
 {
 	var date = new Date();
