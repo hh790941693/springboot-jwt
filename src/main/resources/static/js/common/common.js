4fa1117752aa06a5ce6ae5a52e5d6e87a04e5d90
hch <123456>
2020-05-20 21:17:55 +0800

    优化

--- src/main/resources/static/js/common/common.js
+++ src/main/resources/static/js/common/common.js
@@ -7,8 +7,103 @@ uploadUrl = protocol+"//"+host+"/upload/app";
 //显示图片
 $.ws = {
     gShowImg : function(imgUrl) {
-        console.log("xxxxxxxx imgUrl:" + imgUrl);
         $.fancybox.open('<img src = "' + imgUrl + '" />');
+    },
+
+    backgroundImgAnimation : function(imgId, divId, direction) {
+        var imgWidth = $("#" + imgId).width();
+        var imgHeight = $("#" + imgId).height();
+        var imgUrl = $("#" + imgId).attr("src");
+
+        var divWidth = $("#" + divId).width();
+        var divHeight = $("#" + divId).height();
+
+        var divBackSizeX, divBackSizeY = 0;
+        if (imgWidth < divWidth) {
+            divBackSizeX = divWidth;
+        } else {
+            divBackSizeX = imgWidth;
+        }
+
+        if (imgHeight < divHeight) {
+            divBackSizeY = divHeight;
+        } else {
+            divBackSizeY = imgHeight;
+        }
+        $("#" + divId).css("background-size", divBackSizeX + "px " + divBackSizeY + "px");
+        $("#" + divId).css("background-image", "url('" + imgUrl + "')");
+
+        var maxPosX = imgWidth - divWidth;
+        var maxPosY = imgHeight - divHeight;
+        if (direction == 1) {
+            //上下滚动
+            var posY = 0;  //max -690
+            var downFlag = true;
+            setInterval(function () {
+                if (downFlag) {
+                    posY -= 1;
+                }
+                if (!downFlag) {
+                    posY += 1;
+                }
+                if (posY <= -maxPosY) {
+                    downFlag = false;
+                }
+                if (posY >= 0) {
+                    downFlag = true;
+                }
+                $("#" + divId).css("background-position", "0px " + posY + "px");
+            }, 10);
+        } else if (direction == 2) {
+            //左右滚动
+            var posX = 0;
+            var leftFlag = true;
+            setInterval(function () {
+                if (leftFlag) {
+                    posX -= 1;
+                }
+                if (!leftFlag) {
+                    posX += 1;
+                }
+                if (posX <= -maxPosX) {
+                    leftFlag = false;
+                }
+                if (posX >= 0) {
+                    leftFlag = true;
+                }
+                $("#" + divId).css("background-position", posX + "px 0px");
+            }, 10);
+        } else if (direction == 3) {
+            //圆圈运动
+            var posX = 0;
+            var posY = 0;
+            // 1 右  2下 3 左  4上
+            var direct = 1;
+            setInterval(function () {
+                if (direct == 1) {
+                    posX -= 1;
+                } else if (direct == 2) {
+                    posY -= 1;
+                } else if (direct == 3) {
+                    posX += 1;
+                } else if (direct == 4) {
+                    posY += 1;
+                }
+                if (posX <= -maxPosX && direct == 1) {
+                    direct = 2;
+                }
+                if (posY <= -maxPosY && direct == 2) {
+                    direct = 3;
+                }
+                if (posX >= 0 && direct == 3) {
+                    direct = 4;
+                }
+                if (posY >= 0 && direct == 4) {
+                    direct = 1;
+                }
+                $("#" + divId).css("background-position", posX + "px " + posY + "px");
+            }, 5);
+        }
     }
 }
 
