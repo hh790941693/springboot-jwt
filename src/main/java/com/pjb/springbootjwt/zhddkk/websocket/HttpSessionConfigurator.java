a57fc75928cb9f8bd503b102caa1f5b91353a747
hch <547495788@qq.com>
2020-05-27 13:35:43 +0800

    优化

--- src/main/java/com/pjb/springbootjwt/zhddkk/websocket/HttpSessionConfigurator.java
+++ src/main/java/com/pjb/springbootjwt/zhddkk/websocket/HttpSessionConfigurator.java
@@ -13,6 +13,8 @@ public class HttpSessionConfigurator extends ServerEndpointConfig.Configurator {
     @Override
     public void modifyHandshake(ServerEndpointConfig sec, HandshakeRequest request, HandshakeResponse response) {
         HttpSession httpSession = (HttpSession) request.getHttpSession();
-        sec.getUserProperties().put(HttpSession.class.getName(), httpSession);
+        if (null != httpSession) {
+            sec.getUserProperties().put(HttpSession.class.getName(), httpSession);
+        }
     }
 }
