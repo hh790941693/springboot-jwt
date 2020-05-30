283b777582eaf554ee3f27ec3288ca9b31132627
hch <547495788@qq.com>
2020-05-30 12:01:54 +0800

    优化

---
+++ src/main/java/com/pjb/springbootjwt/zhddkk/dao/WsFeedbackDao.java
@@ -0,0 +1,13 @@
+package com.pjb.springbootjwt.zhddkk.dao;
+
+import com.pjb.springbootjwt.zhddkk.base.BaseDao;
+import com.pjb.springbootjwt.zhddkk.domain.WsFeedbackDO;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 问题反馈
+ */
+@Repository
+public interface WsFeedbackDao extends BaseDao<WsFeedbackDO> {
+
+}
