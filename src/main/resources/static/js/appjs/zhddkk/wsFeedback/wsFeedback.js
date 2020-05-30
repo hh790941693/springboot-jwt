bee565371a5dd6aa008ebe0803dbe563810cabd8
hch <547495788@qq.com>
2020-05-30 13:30:29 +0800

    优化

--- src/main/resources/static/js/appjs/zhddkk/wsFeedback/wsFeedback.js
+++ src/main/resources/static/js/appjs/zhddkk/wsFeedback/wsFeedback.js
@@ -19,7 +19,7 @@ function load() {
 		pagination : true,//设置为true会在底部显示分页条
 		singleSelect : false,//设置为true将禁止多选
 		//contentType : "application/x-www-form-urlencoded",//发送到服务器的数据编码类型
-		pageSize : 10,//如果设置了分页，每页数据条数
+		pageSize : 4,//如果设置了分页，每页数据条数
 		pageNumber : 1,//如果设置了分布，首页页码
 		//search : true,// 是否显示搜索框
 		showColumns : true,// 是否显示内容下拉框（选择显示的列）
