0f3131aed561140bcbfb8e1d44918931c6e50d10
hch <123456>
2020-11-09 22:13:45 +0800

    屏蔽 防止session被禁用

--- src/main/java/com/pjb/springbootjwt/common/shiro/ShiroConfig.java
+++ src/main/java/com/pjb/springbootjwt/common/shiro/ShiroConfig.java
@@ -22,103 +22,103 @@ import java.util.*;
 @Configuration
 public class ShiroConfig {
 
-    @Bean
-    SessionDAO sessionDAO() {
-        MemorySessionDAO sessionDAO = new MemorySessionDAO();
-        return sessionDAO;
-    }
-
-    @Bean
-    public SessionManager sessionManager() {
-        DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
-        Collection<SessionListener> listeners = new ArrayList<SessionListener>();
-        listeners.add(new BDSessionListener());
-        sessionManager.setSessionListeners(listeners);
-        sessionManager.setSessionDAO(sessionDAO());
-        return sessionManager;
-    }
-
-    @Bean
-    public CacheManager getCacheManager() {
-        EhCacheManager cacheManager = new EhCacheManager();
-        cacheManager.setCacheManagerConfigFile("classpath:ehcache.xml");
-        return cacheManager;
-    }
-
-    //将自己的验证方式加入容器
-    @Bean
-    public CustomRealm myShiroRealm() {
-        CustomRealm customRealm = new CustomRealm();
-        customRealm.setCacheManager(getCacheManager());
-        return customRealm;
-    }
-
-    //权限管理，配置主要是Realm的管理认证
-    @Bean
-    public SecurityManager securityManager() {
-        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
-        securityManager.setRealm(myShiroRealm());
-        securityManager.setSessionManager(sessionManager());
-        return securityManager;
-    }
-
-    //Filter工厂，设置对应的过滤条件和跳转条件
-    @Bean
-    public ShiroFilterFactoryBean shiroFilterFactoryBean() {
-        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
-        shiroFilterFactoryBean.setSecurityManager(securityManager());
-//        //登录
-//        shiroFilterFactoryBean.setLoginUrl("/api/user/login");
-//        //首页
-//        shiroFilterFactoryBean.setSuccessUrl("/authc/index");
-//        //错误页面，认证不通过跳转
-//        shiroFilterFactoryBean.setUnauthorizedUrl("/unauthorized");
-//
-//        // 添加jwt过滤器
-//        Map<String, Filter> filterMap = new HashMap<>();
-//        filterMap.put("jwt", new ShiroAuthenticationFilter());
-//        shiroFilterFactoryBean.setFilters(filterMap);
-//
-//
-//        Map<String, String> map = new LinkedHashMap<>();
-//        //登出
-//        map.put("/logout", "logout");
-//        //所有用户经过jwt校验 检查token是否正确
-//        map.put("/authc/*","jwt");  //执行ShiroAuthenticationFilter.isAccessAllowed
-//        //不需要认证
-//        map.put("/anon/*", "anon");
-//        map.put("/api/user/login", "anon");
-//        map.put("/ws/*", "anon");
-//        map.put("/game/*", "anon");
-//        map.put("/canvas/*", "anon");
-//        map.put("/home/*", "anon");
-//
-//        //登录
-//        shiroFilterFactoryBean.setFilterChainDefinitionMap(map);
-        return shiroFilterFactoryBean;
-    }
-
-    @Bean
-    public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
-        return new LifecycleBeanPostProcessor();
-    }
-
-    @Bean
-    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
-        DefaultAdvisorAutoProxyCreator proxyCreator = new DefaultAdvisorAutoProxyCreator();
-        proxyCreator.setProxyTargetClass(true);
-        return proxyCreator;
-    }
-
 //    @Bean
-//    public ShiroDialect shiroDialect() {
-//        return new ShiroDialect();
+//    SessionDAO sessionDAO() {
+//        MemorySessionDAO sessionDAO = new MemorySessionDAO();
+//        return sessionDAO;
 //    }
-
-    @Bean
-    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(SecurityManager securityManager) {
-        AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor = new AuthorizationAttributeSourceAdvisor();
-        authorizationAttributeSourceAdvisor.setSecurityManager(securityManager);
-        return authorizationAttributeSourceAdvisor;
-    }
+//
+//    @Bean
+//    public SessionManager sessionManager() {
+//        DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
+//        Collection<SessionListener> listeners = new ArrayList<SessionListener>();
+//        listeners.add(new BDSessionListener());
+//        sessionManager.setSessionListeners(listeners);
+//        sessionManager.setSessionDAO(sessionDAO());
+//        return sessionManager;
+//    }
+//
+//    @Bean
+//    public CacheManager getCacheManager() {
+//        EhCacheManager cacheManager = new EhCacheManager();
+//        cacheManager.setCacheManagerConfigFile("classpath:ehcache.xml");
+//        return cacheManager;
+//    }
+//
+//    //将自己的验证方式加入容器
+//    @Bean
+//    public CustomRealm myShiroRealm() {
+//        CustomRealm customRealm = new CustomRealm();
+//        customRealm.setCacheManager(getCacheManager());
+//        return customRealm;
+//    }
+//
+//    //权限管理，配置主要是Realm的管理认证
+//    @Bean
+//    public SecurityManager securityManager() {
+//        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+//        securityManager.setRealm(myShiroRealm());
+//        securityManager.setSessionManager(sessionManager());
+//        return securityManager;
+//    }
+//
+//    //Filter工厂，设置对应的过滤条件和跳转条件
+//    @Bean
+//    public ShiroFilterFactoryBean shiroFilterFactoryBean() {
+//        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+//        shiroFilterFactoryBean.setSecurityManager(securityManager());
+////        //登录
+////        shiroFilterFactoryBean.setLoginUrl("/api/user/login");
+////        //首页
+////        shiroFilterFactoryBean.setSuccessUrl("/authc/index");
+////        //错误页面，认证不通过跳转
+////        shiroFilterFactoryBean.setUnauthorizedUrl("/unauthorized");
+////
+////        // 添加jwt过滤器
+////        Map<String, Filter> filterMap = new HashMap<>();
+////        filterMap.put("jwt", new ShiroAuthenticationFilter());
+////        shiroFilterFactoryBean.setFilters(filterMap);
+////
+////
+////        Map<String, String> map = new LinkedHashMap<>();
+////        //登出
+////        map.put("/logout", "logout");
+////        //所有用户经过jwt校验 检查token是否正确
+////        map.put("/authc/*","jwt");  //执行ShiroAuthenticationFilter.isAccessAllowed
+////        //不需要认证
+////        map.put("/anon/*", "anon");
+////        map.put("/api/user/login", "anon");
+////        map.put("/ws/*", "anon");
+////        map.put("/game/*", "anon");
+////        map.put("/canvas/*", "anon");
+////        map.put("/home/*", "anon");
+////
+////        //登录
+////        shiroFilterFactoryBean.setFilterChainDefinitionMap(map);
+//        return shiroFilterFactoryBean;
+//    }
+//
+//    @Bean
+//    public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
+//        return new LifecycleBeanPostProcessor();
+//    }
+//
+//    @Bean
+//    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+//        DefaultAdvisorAutoProxyCreator proxyCreator = new DefaultAdvisorAutoProxyCreator();
+//        proxyCreator.setProxyTargetClass(true);
+//        return proxyCreator;
+//    }
+//
+////    @Bean
+////    public ShiroDialect shiroDialect() {
+////        return new ShiroDialect();
+////    }
+//
+//    @Bean
+//    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(SecurityManager securityManager) {
+//        AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor = new AuthorizationAttributeSourceAdvisor();
+//        authorizationAttributeSourceAdvisor.setSecurityManager(securityManager);
+//        return authorizationAttributeSourceAdvisor;
+//    }
 }
