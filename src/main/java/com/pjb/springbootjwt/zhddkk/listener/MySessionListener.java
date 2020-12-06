1eb5e3d4db20003339c56e531e7d99db586b2952
hch <123456>
2020-12-06 19:25:27 +0800

    富文本编辑器

--- src/main/java/com/pjb/springbootjwt/zhddkk/listener/MySessionListener.java
+++ src/main/java/com/pjb/springbootjwt/zhddkk/listener/MySessionListener.java
@@ -1,22 +1,28 @@
 package com.pjb.springbootjwt.zhddkk.listener;
 
+import org.springframework.stereotype.Component;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
+@Component
 public class MySessionListener implements HttpSessionListener{
 
 	private static SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	
 	@Override
 	public void sessionCreated(HttpSessionEvent se) {
-		System.out.println("session created........"+SDF.format(new Date()));
+		HttpSession session = se.getSession();
+		System.out.println("session created........id:"+session.getId()+" createTime:"+SDF.format(new Date()));
 	}
 
 	@Override
 	public void sessionDestroyed(HttpSessionEvent se) {
-		System.out.println("session destroyed........"+SDF.format(new Date()));
+        HttpSession session = se.getSession();
+        System.out.println("session destroyed........id:"+session.getId()+" createTime:"+SDF.format(new Date()));
 	}
 }
