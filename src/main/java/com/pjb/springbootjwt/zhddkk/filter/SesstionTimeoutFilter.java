f4c54e1bea5d43abbf6b37bb40525276810aa9b4
huangchaohui <547495788@qq.com>
2020-12-08 17:42:46 +0800

    checkStyle

--- src/main/java/com/pjb/springbootjwt/zhddkk/filter/SesstionTimeoutFilter.java
+++ src/main/java/com/pjb/springbootjwt/zhddkk/filter/SesstionTimeoutFilter.java
@@ -3,19 +3,18 @@ package com.pjb.springbootjwt.zhddkk.filter;
 import com.pjb.springbootjwt.zhddkk.bean.SessionInfoBean;
 import com.pjb.springbootjwt.zhddkk.constants.CommonConstants;
 import com.pjb.springbootjwt.zhddkk.util.JsonUtil;
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.*;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@WebFilter(filterName = "sesstionTimeoutFilter",urlPatterns = {"/*"})
+@WebFilter(filterName = "sesstionTimeoutFilter", urlPatterns = {"/*"})
 public class SesstionTimeoutFilter implements Filter {
 
     private static final Logger logger = LoggerFactory.getLogger(SesstionTimeoutFilter.class);
@@ -60,33 +59,35 @@ public class SesstionTimeoutFilter implements Filter {
             ".mp3"));
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
+    public void init(FilterConfig filterConfig) {
         logger.info("============SesstionTimeoutFilter init()============");
     }
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
-        String servletPath = httpServletRequest.getServletPath();
         String contextPath = httpServletRequest.getContextPath();
 
         httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
-        httpServletResponse.setHeader("Access-Control-Allow-Headers", "User-Agent,Origin,Cache-Control,Content-type,Date,Server,withCredentials,AccessToken,username,offlineticket,Authorization");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers",
+                "User-Agent,Origin,Cache-Control,Content-type,Date,Server,withCredentials,AccessToken,username,offlineticket,Authorization");
         httpServletResponse.setHeader("Access-Control-Allow-Credentials", "*");
         httpServletResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
 
-        SessionInfoBean sessionInfoBean = (SessionInfoBean)httpServletRequest.getSession().getAttribute(CommonConstants.SESSION_INFO);
+        SessionInfoBean sessionInfoBean = (SessionInfoBean) httpServletRequest.getSession().getAttribute(CommonConstants.SESSION_INFO);
         String user = sessionInfoBean == null ? "" : sessionInfoBean.getUser();
 
+        String servletPath = httpServletRequest.getServletPath();
         // 是否要过滤 true:过滤 false:不过滤
         boolean filterFlag = false;
-        if (IGNORE_URL_LIST.contains(servletPath) || StringUtils.isNotBlank(user)){
+        if (IGNORE_URL_LIST.contains(servletPath) || StringUtils.isNotBlank(user)) {
             filterFlag = true;
         }
         if (!filterFlag) {
             for (String prefix : IGNORE_URL_PREFIX_LIST) {
-                if (servletPath.startsWith(prefix)){
+                if (servletPath.startsWith(prefix)) {
                     filterFlag = true;
                     break;
                 }
@@ -94,7 +95,7 @@ public class SesstionTimeoutFilter implements Filter {
 
             if (!filterFlag) {
                 for (String suffix : IGNORE_URL_SUFFIX_LIST) {
-                    if (servletPath.endsWith(suffix)){
+                    if (servletPath.endsWith(suffix)) {
                         filterFlag = true;
                         break;
                     }
