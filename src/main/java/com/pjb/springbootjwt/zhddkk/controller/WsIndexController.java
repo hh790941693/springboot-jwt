04b00cf2c76ec31041e456b903102d3cf0130d28
huangchaohui <547495788@qq.com>
2020-12-09 17:45:20 +0800

    checkStyle

--- src/main/java/com/pjb/springbootjwt/zhddkk/controller/WsIndexController.java
+++ src/main/java/com/pjb/springbootjwt/zhddkk/controller/WsIndexController.java
@@ -12,54 +12,67 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * 首页相关页面.
+ */
 @Controller
 @RequestMapping("/index")
 public class WsIndexController {
 
-    private static Logger logger = LoggerFactory.getLogger(WsIndexController.class);
+    private static final Logger logger = LoggerFactory.getLogger(WsIndexController.class);
 
     /**
-     * 获取天气
+     * 获取天气.
      *
-     * @return
+     * @return 天气信息
      */
     @RequestMapping("/grapWetherInfo")
     @ResponseBody
-    public Result<WeatherBean> grapWetherInfo(){
+    public Result<WeatherBean> grapWetherInfo() {
+        logger.info("获取天气信息");
         try {
             WeatherBean weatherBean = WeatherUtil.grapWeatherInfo();
             return Result.ok(weatherBean);
-        }catch (Exception e){
-
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return Result.fail();
     }
 
-    //联系作者
-    @OperationLogAnnotation(type=OperationEnum.PAGE,module=ModuleEnum.OTHER,subModule="",describe="联系作者页面")
+    /**
+     * 联系作者.
+     * @return 联系作者页面
+     */
+    @OperationLogAnnotation(type = OperationEnum.PAGE, module = ModuleEnum.OTHER, subModule = "", describe = "联系作者页面")
     @RequestMapping("/contactAuthor.page")
-    public String contactAuthor(){
+    public String contactAuthor() {
         return "ws/contactAuthor";
     }
 
-    //捐赠作者
-    @OperationLogAnnotation(type=OperationEnum.PAGE,module=ModuleEnum.OTHER,subModule="",describe="捐赠作者页面")
+    /**
+     * 捐赠作者.
+     */
+    @OperationLogAnnotation(type = OperationEnum.PAGE, module = ModuleEnum.OTHER, subModule = "", describe = "捐赠作者页面")
     @RequestMapping("/donate.page")
-    public String donate(){
+    public String donate() {
         return "ws/donate";
     }
 
-    //提出疑问
-    @OperationLogAnnotation(type=OperationEnum.PAGE,module=ModuleEnum.OTHER,subModule="",describe="提出疑问页面")
+    /**
+     * 提出疑问.
+     */
+    @OperationLogAnnotation(type = OperationEnum.PAGE, module = ModuleEnum.OTHER, subModule = "", describe = "提出疑问页面")
     @RequestMapping("/feedbackUs.page")
-    public String feedbackUs(){
+    public String feedbackUs() {
         return "ws/feedbackUs";
     }
 
-    //关于我们
-    @OperationLogAnnotation(type=OperationEnum.PAGE,module=ModuleEnum.OTHER,subModule="",describe="关于我们页面")
+    /**
+     * 关于我们.
+     */
+    @OperationLogAnnotation(type = OperationEnum.PAGE, module = ModuleEnum.OTHER, subModule = "", describe = "关于我们页面")
     @RequestMapping("/aboutUs.page")
-    public String aboutUs(){
+    public String aboutUs() {
         return "ws/aboutUs";
     }
 }
