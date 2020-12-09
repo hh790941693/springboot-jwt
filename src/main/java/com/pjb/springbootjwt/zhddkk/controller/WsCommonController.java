55a5172b951644a359e02a8133166b6fe1e404ba
huangchaohui <547495788@qq.com>
2020-12-09 17:41:36 +0800

    checkStyle

--- src/main/java/com/pjb/springbootjwt/zhddkk/controller/WsCommonController.java
+++ src/main/java/com/pjb/springbootjwt/zhddkk/controller/WsCommonController.java
@@ -1,15 +1,23 @@
 package com.pjb.springbootjwt.zhddkk.controller;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
-
 //import org.apache.shiro.authz.annotation.RequiresPermissions;
 import com.baomidou.mybatisplus.enums.SqlLike;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.mapper.Wrapper;
+import com.baomidou.mybatisplus.plugins.Page;
+import com.pjb.springbootjwt.common.base.AdminBaseController;
+import com.pjb.springbootjwt.common.vo.Result;
 import com.pjb.springbootjwt.zhddkk.annotation.OperationLogAnnotation;
 import com.pjb.springbootjwt.zhddkk.constants.ModuleEnum;
 import com.pjb.springbootjwt.zhddkk.constants.OperationEnum;
+import com.pjb.springbootjwt.zhddkk.domain.WsCommonDO;
+import com.pjb.springbootjwt.zhddkk.service.WsCommonService;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
 import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
@@ -17,25 +25,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
-import com.baomidou.mybatisplus.mapper.EntityWrapper;
-import com.baomidou.mybatisplus.mapper.Wrapper;
-import com.baomidou.mybatisplus.plugins.Page;
-import com.pjb.springbootjwt.common.base.AdminBaseController;
-import com.pjb.springbootjwt.zhddkk.domain.WsCommonDO;
-import com.pjb.springbootjwt.zhddkk.service.WsCommonService;
-import com.pjb.springbootjwt.common.vo.Result;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
- * 
+ * 通用配置.
  */
 @Controller
 @RequestMapping("/zhddkk/wsCommon")
 public class WsCommonController extends AdminBaseController {
 
-    private static Logger logger = LoggerFactory.getLogger(WsCommonController.class);
+    private static final Logger logger = LoggerFactory.getLogger(WsCommonController.class);
 
+    /**
+     * binder.
+     * @param binder binder
+     */
     @InitBinder
     public void initBinder(WebDataBinder binder) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -46,32 +48,32 @@ public class WsCommonController extends AdminBaseController {
     @Autowired
     private WsCommonService wsCommonService;
 
-
     /**
-    * 跳转到页面
+    * 跳转到页面.
     */
-    @OperationLogAnnotation(type=OperationEnum.PAGE,module=ModuleEnum.CONFIGURATION,subModule="",describe="通用配置页面")
+    @OperationLogAnnotation(type = OperationEnum.PAGE, module = ModuleEnum.CONFIGURATION, subModule = "", describe = "通用配置页面")
     @GetMapping("{type}")
     //@RequiresPermissions("zhddkk:wsCommon:wsCommon")
-    public String wsCommon(Model model, @PathVariable("type")String type){
+    public String wsCommon(Model model, @PathVariable("type")String type) {
+        logger.info("进入通用配置页面:{}", type);
         model.addAttribute("type", type);
         return "zhddkk/wsCommon/wsCommon";
     }
 
     /**
-     * 获取列表数据
+     * 获取列表数据.
      */
-    @OperationLogAnnotation(type=OperationEnum.QUERY,module=ModuleEnum.CONFIGURATION,subModule="",describe="通用配置列表")
+    @OperationLogAnnotation(type = OperationEnum.QUERY, module = ModuleEnum.CONFIGURATION, subModule = "", describe = "通用配置列表")
     @ResponseBody
     @GetMapping("/list")
     //@RequiresPermissions("zhddkk:wsCommon:wsCommon")
-    public Result<Page<WsCommonDO>> list(WsCommonDO wsCommonDTO){
+    public Result<Page<WsCommonDO>> list(WsCommonDO wsCommonDtO) {
         Wrapper<WsCommonDO> wrapper = new EntityWrapper<WsCommonDO>();
-        if (StringUtils.isNotBlank(wsCommonDTO.getType())){
-            wrapper.eq("type", wsCommonDTO.getType());
+        if (StringUtils.isNotBlank(wsCommonDtO.getType())) {
+            wrapper.eq("type", wsCommonDtO.getType());
         }
-        if (StringUtils.isNotBlank(wsCommonDTO.getName())){
-            wrapper.like("name", wsCommonDTO.getName(), SqlLike.DEFAULT);
+        if (StringUtils.isNotBlank(wsCommonDtO.getName())) {
+            wrapper.like("name", wsCommonDtO.getName(), SqlLike.DEFAULT);
         }
         wrapper.orderBy("orderby");
         Page<WsCommonDO> page = wsCommonService.selectPage(getPage(WsCommonDO.class), wrapper);
@@ -79,12 +81,12 @@ public class WsCommonController extends AdminBaseController {
     }
 
     /**
-     * 跳转到添加页面
+     * 跳转到添加页面.
      */
-    @OperationLogAnnotation(type=OperationEnum.PAGE,module=ModuleEnum.CONFIGURATION,subModule="",describe="添加配置页面")
+    @OperationLogAnnotation(type = OperationEnum.PAGE, module = ModuleEnum.CONFIGURATION, subModule = "", describe = "添加配置页面")
     @GetMapping("/add")
     //@RequiresPermissions("zhddkk:wsCommon:add")
-    public String add(Model model, String type){
+    public String add(Model model, String type) {
         WsCommonDO wsCommon = new WsCommonDO();
         wsCommon.setType(type);
         model.addAttribute("wsCommon", wsCommon);
@@ -92,62 +94,61 @@ public class WsCommonController extends AdminBaseController {
     }
 
     /**
-     * 跳转到编辑页面
+     * 跳转到编辑页面.
      * @param id ID
      * @param model 实体
      */
     @GetMapping("/edit/{id}")
     //@RequiresPermissions("zhddkk:wsCommon:edit")
-    public String edit(@PathVariable("id") Integer id,Model model){
+    public String edit(@PathVariable("id") Integer id, Model model) {
         WsCommonDO wsCommon = wsCommonService.selectById(id);
         model.addAttribute("wsCommon", wsCommon);
         return "zhddkk/wsCommon/wsCommonForm";
     }
 
     /**
-     * 保存
+     * 保存.
      */
-    @OperationLogAnnotation(type=OperationEnum.INSERT,module=ModuleEnum.CONFIGURATION,subModule="",describe="保存通用配置")
+    @OperationLogAnnotation(type = OperationEnum.INSERT, module = ModuleEnum.CONFIGURATION, subModule = "", describe = "保存通用配置")
     @ResponseBody
     @PostMapping("/save")
     //@RequiresPermissions("zhddkk:wsCommon:add")
-    public Result<String> save( WsCommonDO wsCommon){
+    public Result<String> save(WsCommonDO wsCommon) {
         wsCommonService.insert(wsCommon);
         return Result.ok();
     }
 
     /**
-     * 编辑
+     * 编辑.
      */
     @ResponseBody
     @RequestMapping("/update")
     //@RequiresPermissions("zhddkk:wsCommon:edit")
-    public Result<String> update( WsCommonDO wsCommon){
+    public Result<String> update(WsCommonDO wsCommon) {
         wsCommonService.updateById(wsCommon);
         return Result.ok();
     }
 
     /**
-     * 删除
+     * 删除.
      */
-    @OperationLogAnnotation(type=OperationEnum.DELETE,module=ModuleEnum.CONFIGURATION,subModule="",describe="删除通用配置")
+    @OperationLogAnnotation(type = OperationEnum.DELETE, module = ModuleEnum.CONFIGURATION, subModule = "", describe = "删除通用配置")
     @PostMapping("/remove")
     @ResponseBody
     //@RequiresPermissions("zhddkk:wsCommon:remove")
-    public Result<String> remove( Integer id){
+    public Result<String> remove(Integer id) {
         wsCommonService.deleteById(id);
         return Result.ok();
     }
 
     /**
-     * 批量删除
+     * 批量删除.
      */
     @PostMapping("/batchRemove")
     @ResponseBody
     //@RequiresPermissions("zhddkk:wsCommon:batchRemove")
-    public Result<String> remove(@RequestParam("ids[]") Integer[] ids){
+    public Result<String> remove(@RequestParam("ids[]") Integer[] ids) {
         wsCommonService.deleteBatchIds(Arrays.asList(ids));
         return Result.ok();
     }
-
 }
