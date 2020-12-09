95360e7575aefc04dc42ee7257e6dcb9d2cf5bd9
huangchaohui <547495788@qq.com>
2020-12-09 18:41:07 +0800

    checkStyle

--- src/main/java/com/pjb/springbootjwt/zhddkk/entity/PageResponseEntity.java
+++ src/main/java/com/pjb/springbootjwt/zhddkk/entity/PageResponseEntity.java
@@ -1,72 +1,14 @@
 package com.pjb.springbootjwt.zhddkk.entity;
 
 import java.util.List;
+import lombok.Data;
 
-public class PageResponseEntity 
-{
+@Data
+public class PageResponseEntity {
     private int totalPage;
-
     private int totalCount;
-
     private List<?> list;
-
     private Object parameter1 = "";
-
     private Object parameter2 = "";
-
     private Object parameter3 = "";
-
-    public int getTotalPage() {
-        return totalPage;
-    }
-
-    public void setTotalPage(int totalPage) {
-        this.totalPage = totalPage;
-    }
-
-    public int getTotalCount() {
-        return totalCount;
-    }
-
-    public void setTotalCount(int totalCount) {
-        this.totalCount = totalCount;
-    }
-
-    public List<?> getList() {
-        return list;
-    }
-
-    public void setList(List<?> list) {
-        this.list = list;
-    }
-
-    public Object getParameter1() {
-        return parameter1;
-    }
-
-    public void setParameter1(Object parameter1) {
-        this.parameter1 = parameter1;
-    }
-
-    public Object getParameter2() {
-        return parameter2;
-    }
-
-    public void setParameter2(Object parameter2) {
-        this.parameter2 = parameter2;
-    }
-
-    public Object getParameter3() {
-        return parameter3;
-    }
-
-    public void setParameter3(Object parameter3) {
-        this.parameter3 = parameter3;
-    }
-
-    @Override
-    public String toString() {
-        return "PageResponseEntity [totalPage=" + totalPage + ", totalCount=" + totalCount + ", parameter1="
-                + parameter1 + ", list=" + list + "]";
-    }
 }
--- src/main/java/com/pjb/springbootjwt/zhddkk/entity/WsOnlineInfo.java
+++ src/main/java/com/pjb/springbootjwt/zhddkk/entity/WsOnlineInfo.java
@@ -2,119 +2,30 @@ package com.pjb.springbootjwt.zhddkk.entity;
 
 import com.pjb.springbootjwt.zhddkk.domain.WsCommonDO;
 import com.pjb.springbootjwt.zhddkk.domain.WsUsersDO;
-
+import lombok.Data;
 import java.util.List;
 import java.util.Map;
 
-public class WsOnlineInfo 
-{
+@Data
+public class WsOnlineInfo {
     // 在线人数
     private int onlineCount;
-
     // 离线人数
     private int offlineCount;
-
     // 当前登录的用户信息
     private WsUsersDO currentOnlineUserInfo;
-
     // 所有用户列表
     private List<WsUsersDO> userInfoList;
-
     // 在线用户列表
     private List<WsUsersDO> onlineUserList;
-
     // 离线用户列表
     private List<WsUsersDO> offlineUserList;
-
     // 好友列表
     private List<WsUsersDO> friendsList;
-
     //好友在线数
     private int onlineFriendCount = 0;
-
     //好友离线数
     private int offlineFriendCount = 0;
-
     // 敏感词、脏话、常用语、注册问题
     private Map<String, List<WsCommonDO>> commonMap;
-
-    public int getOnlineCount() {
-        return onlineCount;
-    }
-
-    public void setOnlineCount(int onlineCount) {
-        this.onlineCount = onlineCount;
-    }
-
-    public int getOfflineCount() {
-        return offlineCount;
-    }
-
-    public void setOfflineCount(int offlineCount) {
-        this.offlineCount = offlineCount;
-    }
-
-    public Map<String, List<WsCommonDO>> getCommonMap() {
-        return commonMap;
-    }
-
-    public void setCommonMap(Map<String, List<WsCommonDO>> commonMap) {
-        this.commonMap = commonMap;
-    }
-
-    public WsUsersDO getCurrentOnlineUserInfo() {
-        return currentOnlineUserInfo;
-    }
-
-    public void setCurrentOnlineUserInfo(WsUsersDO currentOnlineUserInfo) {
-        this.currentOnlineUserInfo = currentOnlineUserInfo;
-    }
-
-    public List<WsUsersDO> getUserInfoList() {
-        return userInfoList;
-    }
-
-    public void setUserInfoList(List<WsUsersDO> userInfoList) {
-        this.userInfoList = userInfoList;
-    }
-
-    public List<WsUsersDO> getOnlineUserList() {
-        return onlineUserList;
-    }
-
-    public void setOnlineUserList(List<WsUsersDO> onlineUserList) {
-        this.onlineUserList = onlineUserList;
-    }
-
-    public List<WsUsersDO> getOfflineUserList() {
-        return offlineUserList;
-    }
-
-    public void setOfflineUserList(List<WsUsersDO> offlineUserList) {
-        this.offlineUserList = offlineUserList;
-    }
-
-    public List<WsUsersDO> getFriendsList() {
-        return friendsList;
-    }
-
-    public void setFriendsList(List<WsUsersDO> friendsList) {
-        this.friendsList = friendsList;
-    }
-
-    public int getOnlineFriendCount() {
-        return onlineFriendCount;
-    }
-
-    public void setOnlineFriendCount(int onlineFriendCount) {
-        this.onlineFriendCount = onlineFriendCount;
-    }
-
-    public int getOfflineFriendCount() {
-        return offlineFriendCount;
-    }
-
-    public void setOfflineFriendCount(int offlineFriendCount) {
-        this.offlineFriendCount = offlineFriendCount;
-    }
 }
