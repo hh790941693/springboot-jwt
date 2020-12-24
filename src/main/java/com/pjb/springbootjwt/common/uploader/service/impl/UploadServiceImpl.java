1fa6863adbe0f0c8c608771fe6db09bafff74eaa
huangchaohui <547495788@qq.com>
2020-12-24 17:49:49 +0800

    登录页样式优化

--- src/main/java/com/pjb/springbootjwt/common/uploader/service/impl/UploadServiceImpl.java
+++ src/main/java/com/pjb/springbootjwt/common/uploader/service/impl/UploadServiceImpl.java
@@ -18,54 +18,59 @@ import java.util.UUID;
 
 @Service
 public class UploadServiceImpl implements UploadService {
-
+    
     private static Logger logger = LoggerFactory.getLogger(UploadService.class);
-
+    
     private static String TEMP_PATH = "temp";
-
+    
     @Autowired
     private UploadConfig uploadConfig;
-
+    
     @Autowired
     private WsFileService wsFileService;
-
+    
     @Override
-    public String uploadFile(MultipartFile file, String folder, String userId) throws Exception {
+    public String uploadFile(MultipartFile file, String folder, String userId)
+        throws Exception {
         logger.info("进入上传文件业务层,目录名:{} 用户id:{}", folder, userId);
-        if (StringUtils.isBlank(folder)){
+        if (StringUtils.isBlank(folder)) {
             folder = TEMP_PATH;
         }
         String originFilename = file.getOriginalFilename();
         String newFileName = renameToUUID(file.getOriginalFilename());
-        logger.info("newFileName:"+newFileName);
+        logger.info("newFileName:" + newFileName);
         String totalFolder = uploadConfig.getStorePath() + File.separator + folder + File.separator;
-        logger.info("totalFolder："+totalFolder);
+        logger.info("totalFolder：" + totalFolder);
         File newFile = new File(totalFolder, newFileName);
         FileUtils.writeByteArrayToFile(newFile, file.getBytes());
         String viewUrl = uploadConfig.getViewUrl() + folder + "/" + newFileName;
-
+        
         WsFileDO wsFileDO = new WsFileDO();
         wsFileDO.setUser(userId);
         wsFileDO.setFolder(folder);
         wsFileDO.setFilename(file.getOriginalFilename());
-        wsFileDO.setDiskPath(uploadConfig.getStorePath() + File.separator + folder);
+        if (uploadConfig.getStorePath().endsWith(File.separator)) {
+            wsFileDO.setDiskPath(uploadConfig.getStorePath() + folder);
+        } else {
+            wsFileDO.setDiskPath(uploadConfig.getStorePath() + File.separator + folder);
+        }
         wsFileDO.setUrl(viewUrl);
         wsFileDO.setFileSize(newFile.length());
-
+        
         String trackLength = "00:00";
-//        if (originFilename.endsWith("mp3")) {
-//            try {
-//                trackLength = MusicParserUtil.getMusicTrackTime(newFile.getAbsolutePath());
-//            }catch (Exception e){
-//                logger.info("获取音乐文件时长异常:"+e.getMessage());
-//            }
-//        }
+        // if (originFilename.endsWith("mp3")) {
+        // try {
+        // trackLength = MusicParserUtil.getMusicTrackTime(newFile.getAbsolutePath());
+        // }catch (Exception e){
+        // logger.info("获取音乐文件时长异常:"+e.getMessage());
+        // }
+        // }
         wsFileDO.setTrackLength(trackLength);
         wsFileService.insert(wsFileDO);
-
+        
         return viewUrl;
     }
-
+    
     /**
      * 重命名文件为UUID + 后缀
      *
