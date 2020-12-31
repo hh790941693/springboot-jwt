6127adeed1b77e9c71ac518c470ba6b151bef575
hch <123456>
2020-12-31 20:19:48 +0800

    个人信息增加“区域位置”

--- src/main/resources/static/js/easyui/js/outlook2.js
+++ src/main/resources/static/js/easyui/js/outlook2.js
@@ -1,8 +1,11 @@
-﻿$(function(){
+﻿// 首页tab标题
+var indexTabTitle = "首页";
+$(function(){
 	InitLeftMenu();
 	tabClose();
 	tabCloseEven();
 
+	// 左侧导航栏展开与关闭
 	$(".accordion-header").click(function(){
 		if ($(this).hasClass("accordion-header-selected")) {
 			// 开启
@@ -16,7 +19,6 @@
 
 //初始化左侧
 function InitLeftMenu() {
-
     $(".easyui-accordion").empty();
     var menulist = "";
    
@@ -61,19 +63,19 @@ function addTab(subtitle,url){
 	tabClose();
 }
 
-function createFrame(url)
-{
+function createFrame(url) {
 	var s = '<iframe name="mainFrame" scrolling="auto" frameborder="0"  src="'+url+'" style="width:100%;height:100%;"></iframe>';
 	return s;
 }
 
-function tabClose()
-{
+function tabClose() {
 	/*双击关闭TAB选项卡*/
 	$(".tabs-inner").dblclick(function(){
 		var subtitle = $(this).children("span").text();
-		$('#tabs').tabs('close',subtitle);
-	})
+		if (subtitle != indexTabTitle) {
+            $('#tabs').tabs('close', subtitle);
+        }
+	});
 
 	$(".tabs-inner").bind('contextmenu',function(e){
 		$('#mm').menu('show', {
@@ -88,34 +90,37 @@ function tabClose()
 	});
 }
 //绑定右键菜单事件
-function tabCloseEven()
-{
+function tabCloseEven() {
 	//关闭当前
 	$('#mm-tabclose').click(function(){
-		var currtab_title = $('#mm').data("currtab");
-		$('#tabs').tabs('close',currtab_title);
-	})
+		var currtabTitle = $('#mm').data("currtab");
+		if (currtabTitle != indexTabTitle) {
+            $('#tabs').tabs('close', currtabTitle);
+        }
+	});
 	//全部关闭
 	$('#mm-tabcloseall').click(function(){
 		$('.tabs-inner span').each(function(i,n){
-			var t = $(n).text();
-			$('#tabs').tabs('close',t);
+			var text = $(n).text();
+			if (text != indexTabTitle) {
+                $('#tabs').tabs('close', text);
+            }
 		});	
 	});
 	//关闭除当前之外的TAB
 	$('#mm-tabcloseother').click(function(){
 		var currtab_title = $('#mm').data("currtab");
 		$('.tabs-inner span').each(function(i,n){
-			var t = $(n).text();
-			if(t!=currtab_title)
-				$('#tabs').tabs('close',t);
+			var text = $(n).text();
+			if(text != currtab_title && text != indexTabTitle) {
+                $('#tabs').tabs('close', text);
+            }
 		});	
 	});
 	//关闭当前右侧的TAB
 	$('#mm-tabcloseright').click(function(){
 		var nextall = $('.tabs-selected').nextAll();
-		if(nextall.length==0){
-			//msgShow('系统提示','后边没有啦~~','error');
+		if(nextall.length == 0){
 			alert('后边没有啦~~');
 			return false;
 		}
@@ -128,13 +133,15 @@ function tabCloseEven()
 	//关闭当前左侧的TAB
 	$('#mm-tabcloseleft').click(function(){
 		var prevall = $('.tabs-selected').prevAll();
-		if(prevall.length==0){
+		if(prevall.length == 0){
 			alert('到头了，前边没有啦~~');
 			return false;
 		}
 		prevall.each(function(i,n){
-			var t=$('a:eq(0) span',$(n)).text();
-			$('#tabs').tabs('close',t);
+			var text = $('a:eq(0) span',$(n)).text();
+            if(text != indexTabTitle) {
+                $('#tabs').tabs('close', text);
+            }
 		});
 		return false;
 	});
