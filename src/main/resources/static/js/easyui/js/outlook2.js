0d36e05bcb74e89eb2b52e42876d9767171350c7
hch <123456>
2021-01-01 09:48:12 +0800

    easyui-1.9.11版本增加

--- src/main/resources/static/js/easyui/js/outlook2.js
+++ src/main/resources/static/js/easyui/js/outlook2.js
@@ -20,23 +20,26 @@ $(function(){
 //初始化左侧
 function InitLeftMenu() {
     $(".easyui-accordion").empty();
-    var menulist = "";
-   
+
+    var menujson = {};
     $.each(_menus.menus, function(i, n) {
-        menulist += '<div title="'+n.menuname+'"  icon="'+n.icon+'" style="overflow:auto;">';
-		menulist += '<ul>';
+        menujson = {};
+        menujson.title = n.menuname;
+        menujson.iconCls = n.icon;
+        contentHtml = "<ul>";
         $.each(n.menus, function(j, o) {
-			menulist += '<li><div><a target="mainFrame" href="' + o.url + '" ><span class="icon '+o.icon+'" ></span>' + o.menuname + '</a></div></li> ';
-        })
-        menulist += '</ul></div>';
+            contentHtml += '<li><div><a target="mainFrame" href="' + o.url + '" ><span class="icon '+o.icon+'" ></span>' + o.menuname + '</a></div></li> ';
+        });
+        contentHtml += '</ul>';
+        menujson.content = contentHtml;
+        $('.easyui-accordion').accordion('add',menujson);
     })
 
-	$(".easyui-accordion").append(menulist);
-	
 	$('.easyui-accordion li a').click(function(){
 		var tabTitle = $(this).text();
 		var url = $(this).attr("href");
-		addTab(tabTitle,url);
+		var iconClass = $(this).children("span[class*='icon']").eq(0).attr("class")
+		addTab(tabTitle,url, iconClass);
 		$('.easyui-accordion li div').removeClass("selected");
 		$(this).parent().addClass("selected");
 	}).hover(function(){
@@ -48,14 +51,18 @@ function InitLeftMenu() {
 	$(".easyui-accordion").accordion();
 }
 
-function addTab(subtitle,url){
+function addTab(subtitle,url, iconClass){
 	if(!$('#tabs').tabs('exists',subtitle)){
 		$('#tabs').tabs('add',{
 			title:subtitle,
 			content:createFrame(url),
 			closable:true,
 			width:$('#mainPanle').width()-10,
-			height:$('#mainPanle').height()-26
+			height:$('#mainPanle').height()-26,
+            tools:[{
+                iconCls:iconClass
+            }],
+            toolPosition: 'left'
 		});
 	}else{
 		$('#tabs').tabs('select',subtitle);
