851fa53453bb0470b25de05ad40c34333c7b1ca7
huangchaohui <547495788@qq.com>
2021-05-07 16:33:24 +0900

    优化

--- src/main/java/com/pjb/springbootjwt/zhddkk/listener/ZhddLog4jListener.java
+++ src/main/java/com/pjb/springbootjwt/zhddkk/listener/ZhddLog4jListener.java
@@ -2,15 +2,18 @@ package com.pjb.springbootjwt.zhddkk.listener;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * 日志监听器.
  * 动态注入web项目的安装路径
  */
 public class ZhddLog4jListener implements ServletContextListener {
-    private static final Log logger = LogFactory.getLog(ZhddLog4jListener.class);
+
+    private static final Logger logger = LoggerFactory.getLogger(ZhddLog4jListener.class);
+
     public static final String log4jDirKey = "webAppRootPath";
 
     @Override
