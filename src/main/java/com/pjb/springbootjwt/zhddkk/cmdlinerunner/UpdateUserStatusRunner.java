64d6f7be5332aef815740a97463c98aebda8e8f1
huangchaohui <547495788@qq.com>
2021-05-13 10:24:32 +0900

    缓存数据初始化优化

--- src/main/java/com/pjb/springbootjwt/zhddkk/cmdlinerunner/UpdateUserStatusRunner.java
+++ src/main/java/com/pjb/springbootjwt/zhddkk/cmdlinerunner/UpdateUserStatusRunner.java
@@ -1,20 +1,18 @@
-package com.pjb.springbootjwt.zhddkk.listener;
+package com.pjb.springbootjwt.zhddkk.cmdlinerunner;
 
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.pjb.springbootjwt.zhddkk.domain.WsUsersDO;
 import com.pjb.springbootjwt.zhddkk.service.WsUsersService;
 import com.pjb.springbootjwt.zhddkk.websocket.ZhddWebSocket;
-import java.util.List;
-import java.util.Map;
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
-import javax.websocket.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import javax.websocket.Session;
+import java.util.List;
+import java.util.Map;
 
 /**
  * tomcat启动后更新用户表ws_user 更新state字段为0.
@@ -23,27 +21,18 @@ import org.springframework.web.context.support.WebApplicationContextUtils;
  *
  */
 @Component
-public class ModifyUserStatusListener implements ServletContextListener, CommandLineRunner {
-    private static final Logger logger = LoggerFactory.getLogger(ModifyUserStatusListener.class);
+public class UpdateUserStatusRunner implements CommandLineRunner {
+    private static final Logger logger = LoggerFactory.getLogger(UpdateUserStatusRunner.class);
 
+    @Autowired
     private WsUsersService wsUsersService;
 
     @Override
-    public void contextDestroyed(ServletContextEvent arg0) {
-    }
-
-    @Override
     public void run(String... args) {
-        logger.info("进入ModifyUserStatusListener");
+        logger.info("进入了UpdateUserStatusRunner");
         updateUser();
     }
-
-    @Override
-    public void contextInitialized(ServletContextEvent arg0) {
-        WebApplicationContext springContext = WebApplicationContextUtils.getWebApplicationContext(arg0.getServletContext());
-        wsUsersService = (WsUsersService) springContext.getBean("wsUsersServiceImpl");
-    }
-
+    
     private void updateUser() {
         logger.info("开始检查在线用户状态");
         Map<String, Map<String, Session>> clientsMap = ZhddWebSocket.getClientsMap();
