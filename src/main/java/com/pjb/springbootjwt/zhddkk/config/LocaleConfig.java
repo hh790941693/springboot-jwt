a0b300d32ad5ebb2bb984805ee4a49c943f54cd8
huangchaohui <547495788@qq.com>
2021-05-14 17:33:11 +0900

    session的创建时机优化

--- src/main/java/com/pjb/springbootjwt/zhddkk/config/LocaleConfig.java
+++ src/main/java/com/pjb/springbootjwt/zhddkk/config/LocaleConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
@@ -21,7 +22,11 @@ public class LocaleConfig implements WebMvcConfigurer {
 
     @Bean
     public LocaleResolver localeResolver() {
-        SessionLocaleResolver slr = new SessionLocaleResolver();
+        // 禁用session存储locale,是因为其会主动创建session(尚未登录的情况下不允许先创建session)
+        //SessionLocaleResolver srl = new SessionLocaleResolver();
+
+        // 使用Cookie存储locale
+        CookieLocaleResolver slr = new CookieLocaleResolver();
         // 默认语言
         slr.setDefaultLocale(Locale.SIMPLIFIED_CHINESE);
         return slr;
