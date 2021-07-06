c5910926fc73639dc16c3af9c96097e49c293a4f
huangchaohui <547495788@qq.com>
2021-07-06 17:17:41 +0900

    超市功能

--- src/main/java/com/pjb/springbootjwt/shop/ShopUtil.java
+++ src/main/java/com/pjb/springbootjwt/shop/ShopUtil.java
@@ -33,16 +33,27 @@ public class ShopUtil {
      */
     public static String buildOrderNo(String preFix, String userId) {
         StringBuilder sb = new StringBuilder();
+        //前缀
         if (StringUtils.isNotBlank(preFix)) {
             sb.append(preFix);
         }
+        //用户名
         if (StringUtils.isNotBlank(userId)) {
             sb.append(userId);
         }
+        //4个随机数
         Random random = new Random();
         for (int i=0;i<=3;i++) {
             sb.append(random.nextInt(10));
         }
+        sb.append("-");
+
+        // 当前时间戳后4位
+        long timestamp = System.currentTimeMillis();
+        String timestampStr = String.valueOf(timestamp).substring(9);
+        sb.append(timestampStr).append("-");
+
+        //UUID的hash值
         int hashCode = UUID.randomUUID().toString().hashCode();
         if(hashCode < 0) {
             hashCode = - hashCode;
