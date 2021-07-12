35b495882392e255e99263e9258d78e9666ece79
huangchaohui <547495788@qq.com>
2021-07-12 12:06:29 +0900

    三菱工具

---
+++ src/main/java/com/pjb/springbootjwt/util/CopySqlUtils.java
@@ -0,0 +1,90 @@
+package com.pjb.springbootjwt.util;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CopySqlUtils {
+
+    public static final String TRUNK_BASE_PATH = "G:\\workspace\\mccmcKi\\";
+
+    public static final String XXBF_BASE_PATH = "G:\\pj_mitubishi\\00_document\\04_詳細設計\\202107_変更対応\\現行本番\\20180426_mccmc\\";
+
+    public static final String STORE_BASE_PATH = "C:\\Users\\huangchaohui\\Desktop\\三菱项目资料\\202107月三菱需求\\sql\\转换前\\mccmcKi\\";
+
+    public static final String CC_SQL_PATH = "JavaSource\\jp\\co\\rsi\\mccmc\\cc\\integration\\dao\\impl\\sqlmap\\";
+
+    public static final String COMMON_SQL_PATH = "JavaSource\\jp\\co\\rsi\\mccmc\\common\\dbauto\\integration\\dao\\impl\\sqlmap\\";
+
+    public static final String MC_SQL_PATH = "JavaSource\\jp\\co\\rsi\\mccmc\\mc\\integration\\dao\\impl\\sqlmap\\";
+
+    public static void main(String[] args) {
+        run();
+    }
+
+    public static void run(){
+        List<String> ccFileList = new ArrayList<>(Arrays.asList("CcComExistManager.xml:1","CcComOtherManager.xml:1","CcComPulldownManager.xml:1","CcComSeqNoManager.xml:1"));
+        List<String> commonFileList = new ArrayList<>(Arrays.asList("MccChrgCnv.xml","MccExptImptStjk.xml","MccKikiCarCostKs.xml","MccKyori.xml","MccOthrKeihiStjk.xml","MccShashu.xml","MccShkshCostKs.xml",
+                                                                    "MccTariffFile.xml","MccTariffXaxis.xml","MccTariff.xml","MccTariffYaxisCd.xml","MccTariffYaxisSuchi.xml","MccTmawsCnd.xml","McmAutoNmb.xml",
+                                                                    "McmBinKbn.xml","McmCmnExp.xml","McmCmn.xml","McmGenryo.xml:1","McmHinryaku.xml:1","McmHokanBasho.xml","McmNinushi.xml","McmNisgt.xml","McmNm.xml",
+                                                                    "McmOrg.xml","McmReprHin.xml","McmSp.xml","McmTori.xml","MmcNinushiHaifuDetail.xml:1","MmcNinushiHaifu.xml:1","MsnGenba.xml","MsnWkGrp.xml:1",
+                                                                    "TccMstHenkoKanri.xml","TcmMepExptJchu.xml"
+        ));
+        List<String> mcFileList = new ArrayList<>(Arrays.asList("LCCCM07.xml:1","LCMCM08.xml:1","LCMCM75.xml:1","McComExistManager.xml:1","McComOtherManager.xml:1","McComPulldownManager.xml:1","McComSeqNoManager.xml:1"));
+
+        for (String cc : ccFileList) {
+            if (cc.contains(":")) {
+                String filename=cc.split(":")[0];
+                copy(XXBF_BASE_PATH+CC_SQL_PATH+filename, STORE_BASE_PATH+CC_SQL_PATH+filename);
+            } else {
+                copy(TRUNK_BASE_PATH+CC_SQL_PATH+cc, STORE_BASE_PATH+CC_SQL_PATH+cc);
+            }
+        }
+
+        for (String common : commonFileList) {
+            if (common.contains(":")) {
+                String filename=common.split(":")[0];
+                copy(XXBF_BASE_PATH+COMMON_SQL_PATH+filename, STORE_BASE_PATH+COMMON_SQL_PATH+filename);
+            } else {
+                copy(TRUNK_BASE_PATH+COMMON_SQL_PATH+common, STORE_BASE_PATH+COMMON_SQL_PATH+common);
+            }
+        }
+
+        for (String mc : mcFileList) {
+            if (mc.contains(":")) {
+                String filename=mc.split(":")[0];
+                copy(XXBF_BASE_PATH+MC_SQL_PATH+filename, STORE_BASE_PATH+MC_SQL_PATH+filename);
+            } else {
+                copy(TRUNK_BASE_PATH+MC_SQL_PATH+mc, STORE_BASE_PATH+MC_SQL_PATH+mc);
+            }
+        }
+
+    }
+
+    public static void copy(String source, String dest) {
+        InputStream in = null;
+        OutputStream out = null;
+        try {
+            System.out.println(source + "  -->  " + dest);
+            in = new FileInputStream(source);
+            out = new FileOutputStream(dest);
+            IOUtils.copy(in, out);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != in) {
+                    in.close();
+                }
+                if (out != null) {
+                    out.close();
+                }
+            } catch (Exception e) {
+
+            }
+        }
+    }
+}
---
+++ src/main/java/com/pjb/springbootjwt/util/PkgFileUtils.java
@@ -0,0 +1,68 @@
+package com.pjb.springbootjwt.util;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class PkgFileUtils {
+
+    public static final String PRJ_BASE_PATH = "G:\\workspace\\";
+
+    public static final String STORE_BASE_PATH = "C:\\Users\\huangchaohui\\Desktop\\三菱项目资料\\202107月三菱需求\\package\\";
+
+    public static void main(String[] args){
+        run();
+    }
+
+    public static void run(){
+        File file = new File(STORE_BASE_PATH + "wholeFile.txt");
+        BufferedReader reader = null;
+        String timeDir = new SimpleDateFormat("yyyyMMddHH").format(new Date());
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String tempString = null;
+            // 一次读入一行，直到读入null为文件结束
+            while ((tempString = reader.readLine()) != null) {
+                // 显示行号
+                if (StringUtils.isBlank(tempString)) {
+                    continue;
+                }
+                copy(PRJ_BASE_PATH + tempString, STORE_BASE_PATH + timeDir + "\\" + tempString);
+            }
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (Exception e1) {
+                }
+            }
+        }
+    }
+
+    public static void copy(String source, String dest) {
+        InputStream in = null;
+        OutputStream out = null;
+        String destPathname = dest.substring(0, dest.lastIndexOf("\\"));
+        File destPathFile = new File(destPathname);
+        if (!destPathFile.exists()) {
+            destPathFile.mkdirs();
+        }
+        try {
+            System.out.println(source + "  -->  " + dest);
+            in = new FileInputStream(source);
+            out = new FileOutputStream(dest);
+            IOUtils.copy(in, out);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            IOUtils.closeQuietly(in);
+            IOUtils.closeQuietly(out);
+        }
+    }
+}
