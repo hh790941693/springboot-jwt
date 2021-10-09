2fba8225aecc73816ca5b0a8788504c8c3201c78
hch <123456>
2021-10-09 22:27:26 +0800

    其他

--- src/main/java/com/pjb/springbootjwt/zhddkk/netty/server/NettyConstants.java
+++ src/main/java/com/pjb/springbootjwt/zhddkk/netty/server/NettyConstants.java
@@ -4,5 +4,5 @@ public class NettyConstants {
 
     public static final String SERVER_IP = "127.0.0.1";
 
-    public static final int SERVER_PORT = 10000;
+    public static final int SERVER_PORT = 10001;
 }
