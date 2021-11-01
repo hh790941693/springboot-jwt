e19c577f25ed689c9240e6d943cc1139bc9cc724
huangchaohui <547495788@qq.com>
2021-11-01 15:50:36 +0900

    缓存

--- src/main/java/com/pjb/springbootjwt/zhddkk/job/WsFileAccessCheckJob.java
+++ src/main/java/com/pjb/springbootjwt/zhddkk/job/WsFileAccessCheckJob.java
@@ -21,7 +21,6 @@ import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
