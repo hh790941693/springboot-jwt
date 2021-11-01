1a23c3c0fe5c05a8cc82ff988b7498829ac74c3d
huangchaohui <547495788@qq.com>
2021-11-01 15:50:16 +0900

    缓存

--- src/main/java/com/pjb/springbootjwt/zhddkk/job/WsFileAccessCheckJob.java
+++ src/main/java/com/pjb/springbootjwt/zhddkk/job/WsFileAccessCheckJob.java
@@ -24,11 +24,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 /**
  * 定时更新表中url的ip地址.
  */
-@Configuration
+@Component
 @EnableScheduling
 public class WsFileAccessCheckJob {
 
